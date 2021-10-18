d8658b9fd0918c2219cc95529840e9710f3119a2
petereycoder <fp.pipe.reyes@gmail.com>
2021-10-18 17:14:43 -0500

    Iniciando en Typescript

---
+++ bases/app.js
@@ -0,0 +1,8 @@
+"use strict";
+var msg = "Hola mundo";
+var hero = {
+    name: 'Ironman',
+    edad: 45
+};
+hero.edad = 50;
+console.log(hero.edad + 1);
---
+++ bases/app.ts
@@ -0,0 +1,10 @@
+const msg: string = "Hola mundo";
+
+const hero = {
+    name: 'Ironman',
+    edad: 45
+}
+
+hero.edad = 50;
+
+console.log(hero.edad+1);
