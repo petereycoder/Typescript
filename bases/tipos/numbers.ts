37da30bb3e873811ce1bdc66d532978f0e2e0ae3
petereycoder <fp.pipe.reyes@gmail.com>
2021-10-18 19:11:13 -0500

    Agregando datos number

---
+++ bases/tipos/numbers.ts
@@ -0,0 +1,18 @@
+(()=>{
+
+    let avengers:number = 10;
+
+    console.log(avengers);
+
+    const villans:number = 20;
+
+    if(avengers < villans){
+        console.log("Estamos en problemas");
+    }else{
+        console.log("Estamos salvados");
+    }
+
+    avengers = Number("55");
+    console.log(avengers);
+
+})()
