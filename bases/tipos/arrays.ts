a86c23badc81d3b5920592a61a51ef288e7cf7ca
petereycoder <fp.pipe.reyes@gmail.com>
2021-10-19 15:25:38 -0500

    Arreglos

---
+++ bases/tipos/arrays.ts
@@ -0,0 +1,9 @@
+(()=>{
+
+    //const numbers: (string | number | boolean)[] = [1,2,3,4,5,'6',7,8,9,10];
+    const numbers:number[] = [1,2,3,4,5,6,7,8,9,10];
+
+    const villans = ['Duende Verde','Decepticoms','Malefica'];
+    villans.forEach(v=>console.log(v.toLocaleUpperCase()));
+
+})()
