9c40f401c35c60503a583eaa1c350f1ccf03b336
petereycoder <fp.pipe.reyes@gmail.com>
2021-10-22 10:05:41 -0500

    Tuples

---
+++ bases/tipos/tuples.js
@@ -0,0 +1,8 @@
+"use strict";
+(function () {
+    var hero = ['Shrek', 100, true];
+    hero[0] = 'Fiona';
+    hero[1] = 200;
+    hero[2] = false;
+    console.log(hero);
+})();
---
+++ bases/tipos/tuples.ts
@@ -0,0 +1,12 @@
+(()=>{
+
+    const hero: [string,number,boolean] = ['Shrek',100,true];
+    
+    hero[0] = 'Fiona';
+    hero[1] = 200;
+    hero[2] = false;
+
+    console.log(hero);
+
+
+})()
