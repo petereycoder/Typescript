20ef9c5dd3ab0e1a756ab010e3840f7362079fa8
petereycoder <fp.pipe.reyes@gmail.com>
2021-10-28 20:28:32 -0500

    Ejercicio Práctico 2

--- bases/app.js
+++ bases/app.js
@@ -1,35 +1,64 @@
 "use strict";
 (function () {
+    //? Funciones básicas
+    function sumar(a, b) {
+        return a + b;
+    }
+    var contar = function (heroes) {
+        return heroes.length;
+    };
+    var superHeroes = ["Flash", "Arrow", "Superman", "Linterna Verde"];
+    contar(superHeroes);
+    // Parametros por defecto
+    var llamarBatman = function (llamar) {
+        if (llamar === void 0) { llamar = true; }
+        if (llamar) {
+            console.log("Batiseñal activada");
+        }
+    };
+    llamarBatman();
+    //Rest?
+    var unirheroes = function () {
+        var personas = [];
+        for (var _i = 0; _i < arguments.length; _i++) {
+            personas[_i] = arguments[_i];
+        }
+        return personas.join(", ");
+    };
+    //Tipo funcion
+    var noHaceNada = function (numero, texto, booleano, arreglo) { };
+    // Crear el tipo de funcion que acepte la funcion "noHaceNada"
+    var noHaceNadaTampoco;
+    noHaceNadaTampoco = noHaceNada;
     // Tipos
-    var batman = 'Bruce';
-    var superman = 'Clark';
-    var existe = false;
+    //const batman: string = 'Bruce';
+    //const superman: string = 'Clark';
+    //const existe: boolean = false;
     // Tuplas
-    var parejaHeroes = [batman, superman];
-    var villano = ['Lex Lutor', 5, true];
+    //const parejaHeroes: [ string, string ] = [batman,superman];
+    //const villano: [ string, number, boolean ] = ['Lex Lutor',5,true];
     // Arreglos
-    var aliados = ['Mujer Maravilla', 'Acuaman', 'San', 'Flash'];
+    //const aliados: string [] = ['Mujer Maravilla','Acuaman','San', 'Flash'];
     //Enumeraciones
-    var fuerza;
-    (function (fuerza) {
-        fuerza[fuerza["acuaman"] = 0] = "acuaman";
-        fuerza[fuerza["batman"] = 1] = "batman";
-        fuerza[fuerza["flash"] = 5] = "flash";
-        fuerza[fuerza["superman"] = 100] = "superman";
-    })(fuerza || (fuerza = {}));
-    var fuerzaFlash = fuerza.flash;
-    var fuerzaSuperman = fuerza.superman;
-    var fuerzaBatman = fuerza.batman;
-    var fuerzaAcuaman = fuerza.acuaman;
+    //enum fuerza{
+    //acuaman = 0,
+    //batman = 1,
+    //flash = 5,
+    //superman = 100
+    //}
+    //const fuerzaFlash:fuerza = fuerza.flash;
+    //const fuerzaSuperman:fuerza = fuerza.superman;
+    //const fuerzaBatman:fuerza = fuerza.batman;
+    //const fuerzaAcuaman:fuerza = fuerza.acuaman;
     // Retorno de funciones
-    function activar_batiseñal() {
-        return 'activada';
-    }
-    function pedir_ayuda() {
-        console.log('Auxilio!!!');
-    }
+    //function activar_batiseñal(): string{
+    //return 'activada';
+    //}
+    //function pedir_ayuda(): void{
+    //console.log('Auxilio!!!');
+    //}
     // Aserciones de Tipo
-    var poder = '100';
-    var largoDelPoder = poder.length; //Castear
-    console.log(largoDelPoder);
+    //const poder: any = '100';
+    //const largoDelPoder:number = (poder as string ).length; //Castear
+    //console.log( largoDelPoder );
 })();
--- bases/app.ts
+++ bases/app.ts
@@ -1,45 +1,78 @@
 (() => {
 
+    //? Funciones básicas
+  function sumar ( a:number, b:number ):number{
+    return a + b;
+  }
+
+  const contar = ( heroes:string [] ):number => {
+    return heroes.length;
+  }
+  var superHeroes:string [] =  ["Flash", "Arrow", "Superman", "Linterna Verde"];
+  contar(superHeroes);
+
+  // Parametros por defecto
+  const llamarBatman = ( llamar:boolean = true ):void => {
+    if( llamar ){
+      console.log("Batiseñal activada");
+    }
+  }
+
+  llamarBatman();
+
+  //Rest?
+  const unirheroes = ( ...personas:string[] ):string => {
+    return personas.join(", ");
+  }
+
+  //Tipo funcion
+  const noHaceNada = ( numero:number, texto:string, booleano:boolean, arreglo:string [] ) => {}
+
+  // Crear el tipo de funcion que acepte la funcion "noHaceNada"
+  let noHaceNadaTampoco: (n:number,t:string,b:boolean,a:string []) => void;
+
+  noHaceNadaTampoco = noHaceNada;
+
     // Tipos
-    const batman: string = 'Bruce';
-    const superman: string = 'Clark';
+    //const batman: string = 'Bruce';
+    //const superman: string = 'Clark';
   
-    const existe: boolean = false;
+    //const existe: boolean = false;
   
     // Tuplas
-    const parejaHeroes: [ string, string ] = [batman,superman];
-    const villano: [ string, number, boolean ] = ['Lex Lutor',5,true];
+    //const parejaHeroes: [ string, string ] = [batman,superman];
+    //const villano: [ string, number, boolean ] = ['Lex Lutor',5,true];
   
     // Arreglos
-    const aliados: string [] = ['Mujer Maravilla','Acuaman','San', 'Flash'];
+    //const aliados: string [] = ['Mujer Maravilla','Acuaman','San', 'Flash'];
   
     //Enumeraciones
-    enum fuerza{
-      acuaman = 0,
-      batman = 1,
-      flash = 5,
-      superman = 100
-    }
+    //enum fuerza{
+      //acuaman = 0,
+      //batman = 1,
+      //flash = 5,
+      //superman = 100
+    //}
 
-    const fuerzaFlash:fuerza = fuerza.flash;
-    const fuerzaSuperman:fuerza = fuerza.superman;
-    const fuerzaBatman:fuerza = fuerza.batman;
-    const fuerzaAcuaman:fuerza = fuerza.acuaman;
+    //const fuerzaFlash:fuerza = fuerza.flash;
+    //const fuerzaSuperman:fuerza = fuerza.superman;
+    //const fuerzaBatman:fuerza = fuerza.batman;
+    //const fuerzaAcuaman:fuerza = fuerza.acuaman;
 
     
     // Retorno de funciones
-    function activar_batiseñal(): string{
-      return 'activada';
-    }
+    //function activar_batiseñal(): string{
+      //return 'activada';
+    //}
   
-    function pedir_ayuda(): void{
-      console.log('Auxilio!!!');
-    }
+    //function pedir_ayuda(): void{
+      //console.log('Auxilio!!!');
+    //}
   
     // Aserciones de Tipo
-    const poder: any = '100';
-    const largoDelPoder:number = (poder as string ).length; //Castear
-    console.log( largoDelPoder );
+    //const poder: any = '100';
+    //const largoDelPoder:number = (poder as string ).length; //Castear
+    //console.log( largoDelPoder );
   
   
   })()
