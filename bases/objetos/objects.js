d30a1f6e293573ce32804dd4abab1cb31ba1a796
petereycoder <fp.pipe.reyes@gmail.com>
2021-10-29 19:15:17 -0500

    Objetos

---
+++ bases/objetos/objects.js
@@ -0,0 +1,13 @@
+"use strict";
+(function () {
+    var flash = {
+        name: 'Shrek',
+        age: 25,
+        powers: ['Super veloz', 'Super tragón']
+    };
+    flash = {
+        name: 'Pinocho',
+        age: 60,
+        powers: ['Mentiroso']
+    };
+})();
---
+++ bases/objetos/objects.ts
@@ -0,0 +1,15 @@
+(()=>{
+
+    let flash = {
+        name: 'Shrek',
+        age: 25,
+        powers: ['Super veloz','Super tragón']
+    }
+
+    flash = {
+        name: 'Pinocho',
+        age: 60,
+        powers: ['Mentiroso']
+    }
+
+})()
