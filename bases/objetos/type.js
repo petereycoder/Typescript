242de14fb5989ef973561417308517e310641847
petereycoder <fp.pipe.reyes@gmail.com>
2021-11-05 10:30:04 -0600

    Tipos personalizados

---
+++ bases/objetos/type.js
@@ -0,0 +1,13 @@
+"use strict";
+(function () {
+    var flash = {
+        name: 'Shrek',
+        age: 25,
+        powers: [1, 2]
+    };
+    var superman = {
+        name: 'Clark Kent',
+        age: 21,
+        powers: [1]
+    };
+})();
