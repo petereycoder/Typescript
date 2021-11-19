543621582ffaa8aa49836b47b1fc5de4e5c747ce
petereycoder <fp.pipe.reyes@gmail.com>
2021-11-18 19:37:56 -0600

    Export default y exportación con alias

---
+++ ts-node/src/data/powers.ts
@@ -0,0 +1,18 @@
+export interface Power{
+    id: number;
+    des: string;
+}
+
+
+const powers: Power [] = [
+    {
+        id: 1,
+        des: 'Money'
+    },
+    {
+        id: 2,
+        des: 'Drugs'
+    }
+];
+
+export default powers;
--- ts-node/src/index.ts
+++ ts-node/src/index.ts
@@ -1,8 +1,11 @@
-import { Hero } from './classes/Hero';
+import * as HeroClasses from './classes/Hero';
+import  powers, {Power}  from './data/powers';
 
+const Hero = 123;
 
-const ironman = new Hero('Ironman',1,45);
+const ironman = new HeroClasses.Hero('Ironman',1,45);
 console.log(ironman);
+console.log(powers);
 
 
 
