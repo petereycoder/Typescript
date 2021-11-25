86756085f0411898c00acb966b71a98141839587
petereycoder <fp.pipe.reyes@gmail.com>
2021-11-24 18:59:36 -0600

    Ejemplo aplicado de genéricos

--- ts-node/src/index.ts
+++ ts-node/src/index.ts
@@ -1,3 +1,8 @@
+import { getPokemon } from './generics/get-pokemon';
 
+getPokemon(4)
+    .then( resp => console.log(resp))
+    .catch( error => console.log(error))
+    .finally( () => console.log('Fin del getPokemon'))
 
 
