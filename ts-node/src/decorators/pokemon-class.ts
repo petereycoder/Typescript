71f4ee2d464eb812fb9f77893c67f4ac811d46d8
petereycoder <fp.pipe.reyes@gmail.com>
2021-11-25 21:03:35 -0600

    Factory decorators

--- ts-node/src/decorators/pokemon-class.ts
+++ ts-node/src/decorators/pokemon-class.ts
@@ -2,7 +2,15 @@ function prinToConsole(constructor: Function){
     console.log(constructor);
 }
 
-@prinToConsole
+const prinToConsoleConditional = (print: boolean = false):Function =>{
+    if(print){
+        return prinToConsole;
+    }else{
+        return () => {};
+    }
+}
+
+@prinToConsoleConditional(false)
 export class Pokemon{
     public publicApi: string = 'https://pokeapi.co'
     constructor(
