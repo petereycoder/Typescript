cc1bd1160278295c271173b2dbbffb38b985fe1d
petereycoder <fp.pipe.reyes@gmail.com>
2021-11-29 21:29:09 -0600

    Rest Api con Typescript

--- express-api/index.ts
+++ express-api/index.ts
@@ -1,12 +1,15 @@
-const express = require('express')
+import express from 'express';
+//const express = require('express')
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.json({
-      ok: true,
-      msg: 'Todo salió bien'
+
+  res.status(401).json({
+    ok: false,
+    msg: 'No hay token en la petición'
   })
+
 })
 
 app.listen(port, () => {
